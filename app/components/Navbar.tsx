574518da837606815586b0eb7ebac3e376c5c8e7
justiina <112779447+justiina@users.noreply.github.com>
2024-02-07 21:59:58 +0200

    Pages updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { useRouter, usePathname } from "next/navigation";
 import Link from "next/link";
 import { getCurrentUser } from "@/firebase/firebaseConfig";
 import { logout } from "@/app/components/AuthFunctions";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import { FaRegCalendarAlt } from "react-icons/fa";
@@ -27,41 +27,41 @@ const Navbar = () => {
       icon: <FaRegCalendarAlt className="text-2xl" />,
       title: "Kalenteri",
       route: "/calendar",
-      showSmallScreen: true,
+      showOnSmallScreen: true,
     },
     {
       icon: <FaDog className="text-2xl" />,
       title: "Treeniryhmät",
       route: "/groups",
-      showSmallScreen: false,
+      showOnSmallScreen: false,
     },
     {
       icon: <TbFiles className="text-2xl" />,
       title: "Ohjeet ja tiedostot",
       route: "/instructions",
-      showSmallScreen: true,
+      showOnSmallScreen: true,
     },
     {
       icon: <FaSuitcase className="text-2xl" />,
       title: "Hallitus",
       route: "/board",
-      showSmallScreen: false,
+      showOnSmallScreen: false,
     },
     {
       icon: <GoBellFill className="text-2xl" />,
       title: "Hälytysryhmä",
       route: "/calloutgroup",
-      showSmallScreen: true,
+      showOnSmallScreen: true,
     },
     {
       icon: <FaUserCircle className="text-2xl" />,
       title: "Omat tiedot",
       route: "/userinfo",
-      showSmallScreen: false,
+      showOnSmallScreen: false,
     },
   ];
 
-  const smallNavList = navList.filter((item) => item.showSmallScreen);
+  const smallNavList = navList.filter((item) => item.showOnSmallScreen);
 
   const handleSignOut = () => {
     logout();
@@ -72,55 +72,90 @@ const Navbar = () => {
     <div>
       {currentPathname !== "/" && currentPathname !== "/login" && (
         <>
-          <nav className="hidden md:grid grid-rows-10 min-h-screen bg-grey text-background font-sans text-lg p-4">
-            <div className="row-span-3 flex justify-between">
-              {isOpen && <img src="/images/logo300.png" className="h-40"></img>}
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                className="cursor-pointer flex items-center justify-center h-8 w-8 rounded-full hover:bg-background hover:text-grey"
-              >
-                {isOpen ? (
+          {isOpen ? (
+            <nav className="hidden md:grid grid-rows-10 min-h-screen bg-grey text-background font-sans text-lg p-4">
+              <div className="row-span-3 flex justify-between">
+                <img src="/images/logo300.png" className="h-40"></img>
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  className="cursor-pointer flex items-center justify-center h-8 w-8 rounded-full hover:bg-background hover:text-grey"
+                >
                   <IoIosArrowBack className="text-2x" />
-                ) : (
-                  <IoIosArrowForward className="text-2x" />
-                )}
-              </button>
-            </div>
-            <ul className="row-span-4 flex flex-col justify-evenly">
-              {navList.map(({ icon, title, route }, index) => {
-                return (
-                  <li key={index}>
-                    {isOpen ? (
+                </button>
+              </div>
+              <ul className="row-span-4 flex flex-col justify-evenly">
+                {navList.map(({ icon, title, route }, index) => {
+                  const isActive = currentPathname.startsWith(route);
+                  return (
+                    <li key={index}>
                       <Link
                         href={route}
-                        className="flex gap-4 items-center cursor-pointer rounded-full px-4 py-2 hover:bg-background hover:text-grey"
+                        className={
+                          isActive
+                            ? "flex gap-4 items-center cursor-pointer rounded-full px-4 py-2 bg-background text-grey"
+                            : "flex gap-4 items-center cursor-pointer rounded-full px-4 py-2 hover:bg-background hover:text-grey"
+                        }
                       >
                         {icon}
                         {title}
                       </Link>
-                    ) : (
-                      <Link className="flex w-10 h-10" href={route}>
+                    </li>
+                  );
+                })}
+              </ul>
+              <ul className="row-span-4 flex flex-col justify-evenly">
+                <li>
+                  <button
+                    className="flex gap-4 items-center cursor-pointer rounded-full px-4 py-2 hover:bg-orange"
+                    onClick={handleSignOut}
+                  >
+                    <MdLogout className="text-2xl" />
+                    Kirjaudu ulos
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          ) : (
+            <nav className="hidden md:grid grid-rows-10 min-h-screen w-20 bg-grey text-background font-sans text-lg p-4">
+              <div className="row-span-3 flex justify-between">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  className="cursor-pointer flex items-center justify-center h-8 w-8 rounded-full hover:bg-background hover:text-grey"
+                >
+                  <IoIosArrowForward className="text-2x" />
+                </button>
+              </div>
+              <ul className="row-span-4 flex flex-col justify-evenly">
+                {navList.map(({ icon, route }, index) => {
+                  const isActive = currentPathname.startsWith(route);
+                  return (
+                    <li key={index}>
+                      <Link
+                        className={
+                          isActive
+                            ? "flex items-center justify-center cursor-pointer w-10 h-10 rounded-full bg-background text-grey"
+                            : "flex items-center justify-center cursor-pointer w-10 h-10 rounded-full hover:bg-background hover:text-grey"
+                        }
+                        href={route}
+                      >
                         {icon}
                       </Link>
-                    )}
-                  </li>
-                );
-              })}
-            </ul>
-            <ul className="row-span-3 flex flex-col justify-evenly">
-              <li className="flex gap-4 items-center cursor-pointer rounded-full px-4 py-2 hover:bg-orange">
-                <MdLogout className="text-2xl" />
-                {isOpen ? (
-                  <button onClick={handleSignOut}>Kirjaudu ulos</button>
-                ) : (
+                    </li>
+                  );
+                })}
+              </ul>
+              <ul className="row-span-4 flex flex-col justify-evenly">
+                <li>
                   <button
-                    className="flex h-10 w-10 items-center cursor-pointer rounded-full px-4 py-2 hover:bg-orange"
+                    className="flex items-center justify-center cursor-pointer w-10 h-10 rounded-full hover:bg-orange"
                     onClick={handleSignOut}
-                  ></button>
-                )}
-              </li>
-            </ul>
-          </nav>
+                  >
+                    <MdLogout className="text-2xl" />
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          )}
 
           <nav className="flex justify-between bg-grey text-background p-4">
             <div className="flex items-center justify-center h-10 w-10 cursor-pointer rounded-full hover:bg-background hover:text-grey">
@@ -128,10 +163,15 @@ const Navbar = () => {
               <button onClick={() => setShowHamburger(!showHamburger)}></button>
             </div>
             {smallNavList.map(({ icon, route }, index) => {
+              const isActive = currentPathname.startsWith(route);
               return (
                 <li
                   key={index}
-                  className="flex items-center justify-center h-10 w-10 cursor-pointer rounded-full hover:bg-background hover:text-grey"
+                  className={
+                    isActive
+                      ? "flex items-center justify-center h-10 w-10 cursor-pointer rounded-full bg-background text-grey"
+                      : "flex items-center justify-center h-10 w-10 cursor-pointer rounded-full hover:bg-background hover:text-grey"
+                  }
                 >
                   <Link href={route}>{icon}</Link>
                 </li>
