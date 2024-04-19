22034dabee836998129909c6792210550d4d6b80
justiina <112779447+justiina@users.noreply.github.com>
2024-04-19 14:50:59 +0300

    Server.ts fix

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -12,6 +12,15 @@ export const createClient = () => {
         get(name: string) {
           return cookieStore.get(name)?.value;
         },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options });
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
       },
     },
   );
